b9e6292947af79b621491d8ce68cf75817b7dd3e
Mohini-Warkhede <mohiniwarkhede272002@gmail.com>
2022-10-12 04:22:05 +0530

    CREATED CUSTOMER,ORDER AND VENDOR CLASSES

---
+++ src/com/bootcoding/restaurant/MenuItem.java
@@ -0,0 +1,51 @@
+package com.bootcoding.restaurant;
+
+public class MenuItem {
+    private long menuItemId;
+    private String menuItem;
+    private double price;
+
+    public long getMenuItemId() {
+        return menuItemId;
+    }
+
+    public void setMenuItemId(long menuItemId) {
+        this.menuItemId = menuItemId;
+    }
+
+    public String getMenuItem() {
+        return menuItem;
+    }
+
+    public void setMenuItem(String menuItem) {
+        this.menuItem = menuItem;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isIsveg() {
+        return isveg;
+    }
+
+    public void setIsveg(boolean isveg) {
+        this.isveg = isveg;
+    }
+
+    private String category;
+    private boolean isveg;
+
+}
---
+++ src/com/bootcoding/restaurant/Order.java
@@ -0,0 +1,77 @@
+package com.bootcoding.restaurant;
+
+public class Order {
+    private long orderId;   //orderNumber
+    private double totalAmount;
+    private Vendor vendor;     //Vendor Detail
+
+    public long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(long orderId) {
+        this.orderId = orderId;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public Vendor getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(Vendor vendor) {
+        this.vendor = vendor;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public List<OrderMenuItem> getMenuitems() {
+        return menuitems;
+    }
+
+    public void setMenuitems(List<OrderMenuItem> menuitems) {
+        this.menuitems = menuitems;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    private Customer customer;
+    private String deliveryAddress;
+    private List<OrderMenuItem> menuitems;
+    private Date orderDate;
+    private String orderStatus;
+}
